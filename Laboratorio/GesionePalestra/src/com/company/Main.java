b892a164312053a874157824a0ef05d791ce7bf3
FrancescoVaona <62209950+Scout2912@users.noreply.github.com>
2024-05-08 12:04:27 +0200

    Laboratorio 3 finito

--- Laboratorio/GesionePalestra/src/com/company/Main.java
+++ Laboratorio/GesionePalestra/src/com/company/Main.java
@@ -2,6 +2,8 @@ package com.company;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,10 +12,18 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Main extends Application {
+public class Main extends Application{
+
+
+    public enum Filter { TUTTI, STUDENTI, ATLETI};
+    public enum Order { PER_NOME, PER_ETA};
+    Filter filter = Filter.TUTTI;
+    Order order = Order.PER_NOME;
+
     //region Buttons, spacing
     Button tutti = new Button("Tutti");
     Button studenti = new Button("Studenti");
@@ -34,7 +44,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primarystage) throws Exception{
 
-        //manca da centrare i bottoni nel verde nel topAP
+
         //region BorderPane
 
         //BorderPane
@@ -48,7 +58,7 @@ public class Main extends Application {
         topAP.setBackground(new Background(
                 new BackgroundFill(Color.CADETBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        topAP.setPrefSize(30, 30);
+        //topAP.setPrefSize(30, 30);
 
         //region HBOX in topAP
         //Left
@@ -63,7 +73,10 @@ public class Main extends Application {
         //endregion
 
         AnchorPane.setLeftAnchor(topLeftHB, 10.0);
+        AnchorPane.setTopAnchor(topLeftHB, 7.0);
+        AnchorPane.setBottomAnchor(topLeftHB, 7.0);
         AnchorPane.setRightAnchor(topRightHB, 10.0);
+        AnchorPane.setBottomAnchor(topRightHB, 7.0);
 
         topAP.getChildren().add(topLeftHB);
         topAP.getChildren().add(topRightHB);
@@ -72,8 +85,7 @@ public class Main extends Application {
         //endregion
 
         //region center
-
-        Text text = new Text("Benvenuto nella gestione della palestra");
+        Text text = new Text(composeText());
 
         //endregion
 
@@ -89,6 +101,7 @@ public class Main extends Application {
 
         bottomAP.getChildren().add(bottomRightHB);
         AnchorPane.setRightAnchor(bottomRightHB, 10.0);
+        AnchorPane.setBottomAnchor(bottomRightHB, 5.0);
 
         //endregion
 
@@ -100,16 +113,71 @@ public class Main extends Application {
         //endregion
 
         //region HandlerButtons
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Exiting...");
+                primarystage.close();
+            }
+        });
+        tutti.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                tutti.setDisable(true);
+                studenti.setDisable(false);
+                atleti.setDisable(false);
+                filter = Filter.TUTTI;
+                text.setText(composeText());
 
-        //here code for Handling of Buttons
-
+            }
+        });
+        studenti.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                tutti.setDisable(false);
+                studenti.setDisable(true);
+                atleti.setDisable(false);
+                filter = Filter.STUDENTI;
+                text.setText(composeText());
+            }
+        });
+        atleti.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                tutti.setDisable(false);
+                studenti.setDisable(false);
+                atleti.setDisable(true);
+                filter = Filter.ATLETI;
+                text.setText(composeText());
+            }
+        });
+        perNome.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                perNome.setDisable(true);
+                perEta.setDisable(false);
+                order = Order.PER_NOME;
+                text.setText(composeText());
+            }
+        });
+        perEta.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                perEta.setDisable(true);
+                perNome.setDisable(false);
+                order = Order.PER_ETA;
+                text.setText(composeText());
+            }
+        });
 
         //endregion
 
 
         //main settings
+        tutti.setDisable(true);
+        perNome.setDisable(true);
         primarystage.setTitle("Gestione Palestra");
-        Scene scene = new Scene(bp, 500, 250);
+        Scene scene = new Scene(bp, 700, 450);
         primarystage.setScene(scene);
         primarystage.show();
     }
@@ -126,4 +194,69 @@ public class Main extends Application {
         persone.add(new StudenteAtleta("Vincenzo", "Ferrari", "8", 1997, "UNIVR", Studente.Laurea.TRIENNALE, "atletica", IAtleta.Rilevanza.NAZIONALE));
 
     }
+
+    public String composeText(){
+
+        String testo = "";
+        switch (filter){
+            case TUTTI :
+                if (order == Order.PER_ETA){
+                    Collections.sort(persone, new ComparatorByYear());
+                    for(Persona p : persone){
+                        testo += p.toString() + "\n";
+                    };
+
+                } else if (order == Order.PER_NOME) {
+
+                    Collections.sort(persone);
+                    for(Persona p : persone){
+                        testo += p.toString() + "\n";
+                    };
+                }
+                break;
+            case ATLETI:
+                if (order == Order.PER_ETA){
+                    Collections.sort(persone, new ComparatorByYear());
+                    for(Persona p : persone){
+                        if (p instanceof IAtleta){
+                            testo += p.toString() + "\n";
+                        }
+                    };
+
+                } else if (order == Order.PER_NOME) {
+
+                    Collections.sort(persone);
+                    for(Persona p : persone){
+                        if (p instanceof IAtleta){
+                            testo += p.toString() + "\n";
+                        }
+                    };
+                }
+
+                break;
+            case STUDENTI:
+                if (order == Order.PER_ETA){
+                    Collections.sort(persone, new ComparatorByYear());
+                    for(Persona p : persone){
+                        if (p instanceof Studente){
+                            testo += p.toString() + "\n";
+                        }
+                    };
+
+                } else if (order == Order.PER_NOME) {
+
+                    Collections.sort(persone);
+                    for(Persona p : persone){
+                        if (p instanceof Studente){
+                            testo += p.toString() + "\n";
+                        }
+                    };
+                }
+
+                break;
+        }
+
+        return testo;
+    }
+
 }
